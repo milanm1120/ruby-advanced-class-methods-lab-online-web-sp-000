655c6e134f282c0479bd25c5afd6ab0527bd7a4c
Milan Mody <milanm1120@gmail.com>
2021-03-21 02:34:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,12 +19,12 @@ class Song
   def self.new_by_name(name)
     song = self.new
     song.name = name
-    # song.save
-    # song
   end
 
   def self.create_by_name(name)
-
+    song = self.new
+    song.name = name
+    song.save
   end
 end
 
